1c6526631764bc9543ab7c738867d96ce91b6d93
EnachescuAlin <alinenachescu033@gmail.com>
2014-04-26 02:20:30 +0300

    Added the structure Hand.

--- src/libWhistGame/hand.h
+++ src/libWhistGame/hand.h
@@ -6,5 +6,25 @@
 #ifndef HAND_H
 #define HAND_H
 
+#include "deck.h"
+#include "player.h"
+#include "constants.h"
+
+/**
+ * @struct Hand
+ *
+ * @brief Hand structure. In Hand, player[i] gives cards[i]. The players should
+ *        be added in the order of the bids.
+ *
+ * @var Hand::cards
+ *      Pointer to the cards of the hand.
+ * @var Hand::players
+ *      Pointer to the players of the hand.
+ */
+struct Hand {
+    struct Card *cards[MAX_GAME_PLAYERS];
+    struct Player *players[MAX_GAME_PLAYERS];
+};
+
 #endif
 
