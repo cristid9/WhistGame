8669c392b67aedad7b72d0d05953caf39891da10
EnachescuAlin <alinenachescu033@gmail.com>
2014-04-26 01:05:56 +0300

    Created a new module, player.c

---
+++ src/libWhistGame/player.c
@@ -0,0 +1,8 @@
+/**
+ * @file player.c
+ * @brief In this file are defined functions used for Player-related operations,
+ *        these functions are declared in the header file player.h.
+ */
+
+#include "player.h"
+
