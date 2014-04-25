e227990f0d28a5445bab80d651c960509ffe66ed
EnachescuAlin <alinenachescu033@gmail.com>
2014-04-26 01:25:43 +0300

    Added the structures Card and Deck.

--- src/libWhistGame/deck.h
+++ src/libWhistGame/deck.h
@@ -6,5 +6,35 @@
 #ifndef DECK_H
 #define DECK_H
 
+/**
+ * @struct Card
+ *
+ * @brief Card structure. Structure used to keep information about an card.
+ *
+ * @var Card::suit
+ *      The suit of the card.
+ *      
+ * @var Card::value
+ *      The value of the card.
+ */
+struct Card {
+    enum Suit suit;
+    int value;
+}
+
+/**
+ * @struct Deck
+ *
+ * @brief Deck structure. Structure used to keep pointers to the cards of the 
+ *        deck.
+ *
+ * @var Deck::cards
+ *      Pointer to the cards of the deck.
+ */
+struct Deck {
+    struct Card *cards[DECK_SIZE];
+}
+
+
 #endif
 
