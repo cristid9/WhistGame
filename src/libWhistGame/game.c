338732505147ce991fd357489bf75087d0d3ff44
EnachescuAlin <alinenachescu033@gmail.com>
2014-04-26 01:04:59 +0300

    Created a new module, game.c

---
+++ src/libWhistGame/game.c
@@ -0,0 +1,8 @@
+/**
+ * @file game.c
+ * @brief In this file are defined functions used for Game-related operations,
+ *        these functions are declared in the header file game.h.
+ */
+
+#include "game.h"
+
