0010e031fecfd317b919afc24f662d409538ed8c
EnachescuAlin <alinenachescu033@gmail.com>
2014-04-28 21:50:18 +0300

    Added the tests for deck_createDeck().

--- test/test-deck.c
+++ test/test-deck.c
@@ -28,3 +28,33 @@ void test_deck_deleteCard()
     cut_assert_equal_int(NO_ERROR, deck_deleteCard(card));
 }
 
+void test_deck_createDeck()
+{
+    struct Deck *deck;
+    cut_assert_equal_pointer(NULL, deck_createDeck(MAX_GAME_PLAYERS + 1));
+    cut_assert_equal_pointer(NULL, deck_createDeck(MIN_GAME_PLAYERS - 1));
+
+    for (int i = MIN_GAME_PLAYERS; i <= MAX_GAME_PLAYERS; i++) {
+        deck = deck_createDeck(i);
+        int check = 0;
+        if (deck == NULL)
+            check++;
+        cut_assert_equal_int(0, check);
+
+        int k = 0;
+        while (deck->cards[k] != NULL)
+            k++;
+        cut_assert_equal_int((double)MAX_CARDS, (double)++k / i);
+        
+        int duplicates = 0;
+        for (int j = 0; j < DECK_SIZE; j++)
+            for (k = 0; k < DECK_SIZE; k++)
+                if (k != j && deck->cards[k] != NULL && deck->cards[j] != NULL
+                    && deck->cards[k]->suit == deck->cards[j]->suit &&
+                    deck->cards[k]->value == deck->cards[j]->value)
+                    duplicates++;
+        cut_assert_equal_int(0, duplicates);
+    }
+
+}
+
