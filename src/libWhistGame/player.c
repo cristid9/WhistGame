14b908304114d60551ed6283319c9d91b875f820
EnachescuAlin <alinenachescu033@gmail.com>
2014-04-30 23:28:46 +0300

    Added player_createPlayer().

--- src/libWhistGame/player.c
+++ src/libWhistGame/player.c
@@ -6,3 +6,29 @@
 
 #include "player.h"
 
+#include <string.h>
+#include <stdlib.h>
+
+struct Player *player_createPlayer(char *name, int isHuman)
+{
+    if (name == NULL)
+        return NULL;
+
+    struct Player *player = malloc(sizeof(struct Player));
+    if (player == NULL)
+        return NULL;
+
+    player->name = malloc((strlen(name) + 1) * sizeof(char));
+    if (player->name != NULL)
+        strcpy(player->name, name);
+    else
+        return NULL;
+
+    player->isHuman = isHuman;
+
+    for (int i = 0; i < MAX_CARDS; i++)
+        player->hand[i] = NULL;
+
+    return player;
+}
+
