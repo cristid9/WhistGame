8b45c9def67e71ca2aa13e0cbb852aa7d6ad9fb7
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-03 00:45:53 +0300

    Added new errors.

--- src/libWhistGame/errors.h
+++ src/libWhistGame/errors.h
@@ -15,12 +15,15 @@
  *        functions used in this game.
  */
 enum ERROR_CODES {
-    NO_ERROR    =  0,
-    CARD_NULL   = -1,
-    DECK_NULL   = -2,
-    PLAYER_NULL = -3,
-    DUPLICATE   = -4,
-    FULL        = -5
+    NO_ERROR     =  0,
+    CARD_NULL    = -1,
+    DECK_NULL    = -2,
+    PLAYER_NULL  = -3,
+    DUPLICATE    = -4,
+    FULL         = -5,
+    HAND_NULL    = -6,
+    NOT_FOUND    = -7,
+    POINTER_NULL = -8
 };
 
 #endif
