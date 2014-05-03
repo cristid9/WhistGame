daf4a34d7294553a8b109a36c0288b8c6e6fce1d
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-03 23:39:28 +0300

    Added round_createRound().

--- src/libWhistGame/round.c
+++ src/libWhistGame/round.c
@@ -6,3 +6,25 @@
 
 #include "round.h"
 
+struct Round *round_createRound(int roundType)
+{
+    if (roundType < 1 || roundType > 8)
+        return NULL;
+
+    struct Round *round = malloc(sizeof(struct Round));
+    if (round == NULL)
+        return NULL;
+
+    for (int i = 0; i < MAX_GAME_PLAYERS; i++) {
+        round->bids[i]         = 0;
+        round->pointsNumber[i] = 0;
+        round->players[i]      = NULL;
+    }
+
+    round->hand      = NULL;
+    round->trump     = NULL;
+    round->roundType = roundType;
+
+    return round;
+}
+
