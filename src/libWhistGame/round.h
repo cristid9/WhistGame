06cd401aac1e1725239660a1cb348e6fe9bbf5b7
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-04 16:56:48 +0300

    Added the declarations and the documentation for round_createRound(), round_deleteRound(), round_addPlayer(), round_addHand(), round_addTrump().

--- src/libWhistGame/round.h
+++ src/libWhistGame/round.h
@@ -37,5 +37,53 @@ struct Round {
     struct Hand *hand;
 };
 
+/**
+ * @brief Function allocates and initializes a round.
+ *
+ * @param roundType The round type (1, 2, 3, 4, 5, 6, 7, 8).
+ *
+ * @return Pointer to the new round on success, NULL on failure.
+ */
+struct Round *round_createRound(int roundType);
+
+/**
+ * @brief Function releases the memory of the round.
+ *
+ * @param round Pointer to pointer to the round which to be released.
+ *
+ * @return NO_ERROR or 0 on success, other value on failure.
+ */
+int round_deleteRound(struct Round **round);
+
+/**
+ * @brief Function adds a player in a round.
+ *
+ * @param round Pointer to the round in which is added the player.
+ * @param player Pointer to the player which to be added.
+ *
+ * @return NO_ERROR or 0 on success, other value on failure.
+ */
+int round_addPlayer(struct Round *round, struct Player *player);
+
+/**
+ * @brief Function adds a hand in a round.
+ *
+ * @param round Pointer to the round in which is added the hand.
+ * @param hand Pointer to the hand which to be added.
+ *
+ * @return NO_ERROR or 0 on success, other value on failure.
+ */
+int round_addHand(struct Round *round, struct Hand *hand);
+
+/**
+ * @brief Function adds the trump in a round.
+ *
+ * @param round Pointer to the round in which is added the trump.
+ * @param trump Pointer to the card which to be added as trump.
+ *
+ * @return NO_ERROR or 0 on succes, other value on failure.
+ */
+int round_addTrump(struct Round *round, struct Card *trump);
+
 #endif
 
