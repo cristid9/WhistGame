eeb71e51ac0be0e306337b7eedb4150528a464f2
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-11 22:31:53 +0300

    Added game_createGame().

--- src/libWhistGame/game.c
+++ src/libWhistGame/game.c
@@ -6,3 +6,28 @@
 
 #include "game.h"
 
+#include <stdlib.h>
+
+struct Game *game_createGame(int playersNumber, int gameType)
+{
+    if (playersNumber < MIN_GAME_PLAYERS || playersNumber > MAX_GAME_PLAYERS)
+        return NULL;
+    if (gameType != 1 && gameType != 8)
+        return NULL;
+
+    struct Game *game = malloc(sizeof(struct Game));
+    if (game == NULL)
+        return NULL;
+
+    game->playersNumber = playersNumber;
+    game->gameType      = gameType;
+    game->deck          = NULL;
+
+    for (int i = 0; i < MAX_GAME_PLAYERS; i++)
+        game->players[i] = NULL;
+    for (int i = 0; i < MAX_GAME_ROUNDS; i++)
+        game->rounds[i] = NULL;
+
+    return game;
+}
+
