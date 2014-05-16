dff325357a45482d386ae4c8fd3c99b3902e9630
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-16 21:49:44 +0300

    Added the tests for game_addDeck().

--- test/test-game.c
+++ test/test-game.c
@@ -40,3 +40,20 @@ void test_game_deleteGame()
     cut_assert_equal_pointer(NULL, game);
 }
 
+void test_game_addDeck()
+{
+    struct Game *game = game_createGame(MIN_GAME_PLAYERS, 1);
+    struct Deck *deck = deck_createDeck(MIN_GAME_PLAYERS);
+    struct Deck *deck2 = deck;
+
+    cut_assert_equal_int(GAME_NULL, game_addDeck(NULL, &deck));
+    cut_assert_equal_int(POINTER_NULL, game_addDeck(game, NULL));
+    cut_assert_equal_int(NO_ERROR, game_addDeck(game, &deck));
+    cut_assert_equal_pointer(deck2, game->deck);
+    cut_assert_equal_pointer(NULL, deck);
+    cut_assert_equal_int(DECK_NULL, game_addDeck(game, &deck));
+
+    deck2 = NULL;
+    game_deleteGame(&game);
+}
+
