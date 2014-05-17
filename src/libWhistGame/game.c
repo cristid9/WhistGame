784302374e21a71fdab52e14236b35d98bfb72cc
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-17 17:22:11 +0300

    Removed the parameter playersNumber from game_createGame().

--- src/libWhistGame/game.c
+++ src/libWhistGame/game.c
@@ -10,10 +10,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Game *game_createGame(int playersNumber, int gameType)
+struct Game *game_createGame(int gameType)
 {
-    if (playersNumber < MIN_GAME_PLAYERS || playersNumber > MAX_GAME_PLAYERS)
-        return NULL;
     if (gameType != 1 && gameType != 8)
         return NULL;
 
@@ -21,7 +19,7 @@ struct Game *game_createGame(int playersNumber, int gameType)
     if (game == NULL)
         return NULL;
 
-    game->playersNumber = playersNumber;
+    game->playersNumber = 0;
     game->gameType      = gameType;
     game->deck          = NULL;
 
