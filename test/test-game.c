132a5812b85b49833ca231d1e62a7404718b3f5b
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-17 17:24:33 +0300

    Fixed the calls to the function game_createGame() because the parameter playersNumber was removed.

--- test/test-game.c
+++ test/test-game.c
@@ -5,7 +5,7 @@
 
 void test_game_createGame()
 {
-    struct Game *game = game_createGame(MIN_GAME_PLAYERS, 1);
+    struct Game *game = game_createGame(1);
 
     int check = 0;
     if (game != NULL)
@@ -17,23 +17,19 @@ void test_game_createGame()
     for (int i = 0; i < MAX_GAME_ROUNDS; i++)
         cut_assert_equal_pointer(NULL, game->rounds[i]);
 
-    cut_assert_equal_int(MIN_GAME_PLAYERS, game->playersNumber);
+    cut_assert_equal_int(0, game->playersNumber);
     cut_assert_equal_int(1, game->gameType);
     cut_assert_equal_pointer(NULL, game->deck);
 
     game_deleteGame(&game);
 
-    cut_assert_equal_pointer(NULL, game_createGame(MIN_GAME_PLAYERS - 1, 1));
-    cut_assert_equal_pointer(NULL, game_createGame(MAX_GAME_PLAYERS + 1, 1));
-    cut_assert_equal_pointer(NULL, game_createGame(MIN_GAME_PLAYERS, 0));
-    cut_assert_equal_pointer(NULL, game_createGame(MIN_GAME_PLAYERS, 9));
-    cut_assert_equal_pointer(NULL, game_createGame(MIN_GAME_PLAYERS - 1, 5));
-    cut_assert_equal_pointer(NULL, game_createGame(MAX_GAME_PLAYERS + 1, 5));
+    cut_assert_equal_pointer(NULL, game_createGame(0));
+    cut_assert_equal_pointer(NULL, game_createGame(9));
 }
 
 void test_game_deleteGame()
 {
-    struct Game *game = game_createGame(MIN_GAME_PLAYERS, 1);
+    struct Game *game = game_createGame(1);
     cut_assert_equal_int(POINTER_NULL, game_deleteGame(NULL));
     cut_assert_equal_int(NO_ERROR, game_deleteGame(&game));
     cut_assert_equal_int(GAME_NULL, game_deleteGame(&game));
@@ -42,7 +38,7 @@ void test_game_deleteGame()
 
 void test_game_addDeck()
 {
-    struct Game *game = game_createGame(MIN_GAME_PLAYERS, 1);
+    struct Game *game = game_createGame(1);
     struct Deck *deck = deck_createDeck(MIN_GAME_PLAYERS);
     struct Deck *deck2 = deck;
 
@@ -70,7 +66,7 @@ void *getName(int i, char name[4])
 
 void test_game_addPlayer()
 {
-    struct Game *game = game_createGame(MAX_GAME_PLAYERS, 1);
+    struct Game *game = game_createGame(1);
     struct Player *player1;
     struct Player *player2;
     char name[4];
