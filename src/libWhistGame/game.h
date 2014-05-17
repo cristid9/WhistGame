749443eba773a4529a487dfedce16114f69b5a2a
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-17 17:23:12 +0300

    Modified the declaration and the documentation of the function game_createGame() because the parameter playersNumber was removed.

--- src/libWhistGame/game.h
+++ src/libWhistGame/game.h
@@ -38,12 +38,11 @@ struct Game {
 /**
  * @brief Function allocates and initializes a game.
  *
- * @param playersNumber The number of the players which plays in this game.
  * @param gameType The type of the game (1 or 8).
  *
  * @return Pointer to the new game on success, NULL on failure.
  */
-struct Game *game_createGame(int playersNumber, int gameType);
+struct Game *game_createGame(int gameType);
 
 /**
  * @brief Function releases the memory a game and makes pointer NULL.
