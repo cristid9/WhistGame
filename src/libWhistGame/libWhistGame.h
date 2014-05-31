5e9af8cf0d4ad0aed4b5da1148acbab53b013adc
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-31 12:32:07 +0300

    Added all the headers used in libWhistGame. Now when is used libWhistGame must to be included just libWhistGame.h.

---
+++ src/libWhistGame/libWhistGame.h
@@ -0,0 +1,11 @@
+#ifndef LIBWHISTGAME_H
+#define LIBWHISTGAME_H
+
+#include "constants.h"
+#include "errors.h"
+#include "round.h"
+#include "game.h"
+#include "deck.h"
+#include "player.h"
+
+#endif
