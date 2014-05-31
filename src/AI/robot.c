aae48dfe45207aa25273ab5565e6b4cf7fc2c3e4
EnachescuAlin <alinenachescu033@gmail.com>
2014-05-31 12:41:51 +0300

    Created robot.c.

---
+++ src/AI/robot.c
@@ -0,0 +1,8 @@
+/**
+ * @file robot.c
+ * @brief In this file are defined the functions used at the logic of the robot,
+          these functions are defined in the header file robot.c.
+ */
+
+#include "robot.h"
+
