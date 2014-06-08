1c9fc3062b610d9ae4ba3a447c610fbc43ce7e0b
EnachescuAlin <alinenachescu033@gmail.com>
2014-06-09 00:03:58 +0300

    Added robot_getCard().

--- src/AI/robot.c
+++ src/AI/robot.c
@@ -61,3 +61,125 @@ int robot_getBid(struct Player *player, struct Round *round)
         return bids - 1;
 }
 
+int robot_getCard(struct Player *player, struct Round *round)
+{
+    if (player == NULL)
+        return PLAYER_NULL;
+    if (round == NULL)
+        return ROUND_NULL;
+    if (round->hand == NULL)
+        return HAND_NULL;
+
+    int playerId = round_getPlayerId(round, player);
+    if (playerId < 0)
+        return playerId;
+
+    if (round->hand->players[0] == player) {
+        if (round->bids[playerId] > round->handsNumber[playerId]) {
+            int max = 0;
+            int position = 0;
+            for (int i = 0; i < MAX_CARDS; i++)
+                if (player->hand[i] != NULL) {
+                    if (player->hand[i]->value > max) {
+                        max = player->hand[i]->value;
+                        position = i;
+                    }
+                }
+            return position;
+        } else {
+            int min = 15;
+            int position  = 0;
+            for (int i = 0; i < MAX_CARDS; i++)
+                if (player->hand[i] != NULL)
+                    if (player->hand[i]->value < min) {
+                        min = player->hand[i]->value;
+                        position = i;
+                    }
+            return position;
+        }
+    }
+
+    int numberOfTrumps       =  0;
+    int maxValueTrump        =  0;
+    int posMaxValueTrump     = -1;
+    int minValueTrump        = 15;
+    int posMinValueTrump     = -1;
+
+    int numberOfFirstCard    =  0;
+    int maxValueFirstCard    =  0;
+    int posMaxValueFirstCard = -1;
+    int minValueFirstCard    = 15;
+    int posMinValueFirstCard = -1;
+
+    for (int i = 0; i < MAX_CARDS; i++) {
+        if (player->hand[i] != NULL) {
+            if (round->trump != NULL)
+                if (player->hand[i]->suit == round->trump->suit) {
+                    numberOfTrumps++;
+
+                    if (maxValueTrump < player->hand[i]->value) {
+                        maxValueTrump = player->hand[i]->value;
+                        posMaxValueTrump = i;
+                    }
+
+                    if (minValueTrump > player->hand[i]->value) {
+                        minValueTrump = player->hand[i]->value;
+                        posMinValueTrump = i;
+                    }
+                }
+
+            if (player->hand[i]->suit == round->hand->cards[0]->suit) {
+                numberOfFirstCard++;
+
+                if (maxValueFirstCard < player->hand[i]->value) {
+                    maxValueFirstCard = player->hand[i]->value;
+                    posMaxValueFirstCard = i;
+                }
+
+                if (minValueFirstCard > player->hand[i]->value) {
+                    minValueFirstCard = player->hand[i]->value;
+                    posMinValueFirstCard = i;
+                }
+            }
+        }
+    }
+
+    if (round->bids[playerId] > round->handsNumber[playerId]) {
+        if (numberOfFirstCard > 0) {
+            return posMaxValueFirstCard;
+        } else {
+            if (numberOfTrumps > 0) {
+                return posMaxValueTrump;
+            } else {
+                int min = 15;
+                int pos = 0;
+                for (int i = 0; i < MAX_CARDS; i++)
+                    if (player->hand[i] != NULL)
+                        if (player->hand[i]->value < min) {
+                            min = player->hand[i]->value;
+                            pos = i;
+                        }
+                return pos;
+            }
+        }
+    } else {
+        if (numberOfFirstCard > 0) {
+            return posMinValueFirstCard;
+        } else {
+            if (numberOfTrumps > 0) {
+                return posMinValueTrump;
+            } else {
+                int max = 0;
+                int pos = 0;
+                for (int i = 0; i < MAX_CARDS; i++)
+                    if (player->hand[i] != NULL)
+                        if (player->hand[i]->value > max) {
+                            max = player->hand[i]->value;
+                            pos = i;
+                        }
+                return pos;
+            }
+        }
+    }
+}
+
