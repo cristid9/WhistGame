584b4c8e1487e42f0c3e8d31e1a3360d1359f8bf
EnachescuAlin <alinenachescu033@gmail.com>
2014-06-23 22:31:35 +0300

    Added init(), gameType(), playerName(), noOfBots().

--- src/WhistGameGUI/gui.c
+++ src/WhistGameGUI/gui.c
@@ -5,3 +5,86 @@
 
 #include "gui.h"
 
+int init(GtkWidget **window, GtkWidget **fixed, char *title, 
+         int width, int height)
+{
+    if (window == NULL || fixed == NULL || title == NULL)
+        return POINTER_NULL;
+
+    *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_resizable(GTK_WINDOW(*window), FALSE);
+    gtk_widget_set_size_request(*window, width, height);
+    gtk_window_set_position(GTK_WINDOW(*window), GTK_WIN_POS_CENTER);
+    gtk_window_set_title(GTK_WINDOW(*window), title);
+    g_signal_connect(G_OBJECT(*window), "destroy",
+                     G_CALLBACK(gtk_main_quit), NULL);
+    *fixed = gtk_fixed_new();
+    gtk_container_add(GTK_CONTAINER(*window), *fixed);
+    gtk_widget_show(*fixed);
+    gtk_widget_show(*window);
+
+    return NO_ERROR;
+}
+
+int noOfBots(GtkWidget *window, GtkWidget *fixed, GtkWidget **label,
+             GtkWidget **spinNumber, GtkAdjustment **number)
+{
+    if (window == NULL || fixed == NULL || label == NULL ||
+        number == NULL || spinNumber == NULL)
+        return POINTER_NULL;
+
+    *label = gtk_label_new("Choose the robots number");
+    gtk_fixed_put(GTK_FIXED(fixed), *label, 10, 110);
+    gtk_widget_show(*label);
+
+    *number = GTK_ADJUSTMENT(gtk_adjustment_new(2.0, 2.0, 5.0, 1.0, 0.0, 0.0));
+    *spinNumber = gtk_spin_button_new(*number, 1.0, 0);
+    gtk_fixed_put(GTK_FIXED(fixed), *spinNumber, 10, 130);
+    gtk_widget_show(*spinNumber);
+
+    return NO_ERROR;
+}
+
+int playerName(GtkWidget *window, GtkWidget *fixed,
+                GtkWidget **label, GtkWidget **name)
+{
+    if (window == NULL || fixed == NULL || label == NULL || name == NULL)
+        return POINTER_NULL;
+
+    *label = gtk_label_new("Name ");
+    gtk_fixed_put(GTK_FIXED(fixed), *label, 10, 15);
+    gtk_widget_show(*label);
+
+    *name = gtk_entry_new();
+    gtk_entry_set_max_length(GTK_ENTRY(*name), 10);
+    gtk_fixed_put(GTK_FIXED(fixed), *name, 50, 10);
+    gtk_widget_show(*name);
+
+    return NO_ERROR;
+}
+
+int gameType(GtkWidget *window, GtkWidget *fixed, GtkWidget **label,
+              GtkWidget **radio1, GtkWidget **radio8, GtkWidget **vbox)
+{
+    if (window == NULL || fixed == NULL || label == NULL || radio1 == NULL ||
+        radio8 == NULL || vbox == NULL)
+        return POINTER_NULL;
+
+    *label = gtk_label_new("Choose the game type");
+    gtk_fixed_put(GTK_FIXED(fixed), *label, 10, 40);
+    gtk_widget_show(*label);
+
+    *radio1 = gtk_radio_button_new_with_label(NULL, "11-88-11");
+    *radio8 = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(*radio1), "88-11-88");
+    gtk_widget_show(*radio1);
+    gtk_widget_show(*radio8);
+
+    *vbox = gtk_vbox_new(FALSE, 5);
+    gtk_box_pack_start_defaults(GTK_BOX(*vbox), *radio1);
+    gtk_box_pack_start_defaults(GTK_BOX(*vbox), *radio8);
+    gtk_fixed_put(GTK_FIXED(fixed), *vbox, 10, 60);
+    gtk_widget_show(*vbox);
+
+    return NO_ERROR;
+}
+
