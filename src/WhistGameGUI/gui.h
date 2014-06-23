28124b03465308b804ad0e9577420a2b7edd208e
EnachescuAlin <alinenachescu033@gmail.com>
2014-06-23 14:39:33 +0300

    Created gui.h.

---
+++ src/WhistGameGUI/gui.h
@@ -0,0 +1,14 @@
+/**
+ * @file gui.h
+ * @brief In this file are declared the functions used at GUI.
+ */
+
+#ifndef GUI_H
+#define GUI_H
+
+#include <libWhistGameAI.h>
+#include <libWhistGame.h>
+#include <gtk/gtk.h>
+
+#endif
+
