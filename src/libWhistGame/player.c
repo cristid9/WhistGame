fda83e060342c642245ead784250e6aadd9a5931
EnachescuAlin <alinenachescu033@gmail.com>
2014-06-24 19:01:23 +0300

    Now player_createPlayer() has the first parameter const.

--- src/libWhistGame/player.c
+++ src/libWhistGame/player.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct Player *player_createPlayer(char *name, int isHuman)
+struct Player *player_createPlayer(const char *name, int isHuman)
 {
     if (name == NULL)
         return NULL;
