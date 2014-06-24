9c356d1778ea691c4d8da23a736489eea59391a4
EnachescuAlin <alinenachescu033@gmail.com>
2014-06-25 00:16:42 +0300

    Added gui_createButtonShowScore(), gui_getPictureName(), gui_destroyTrump(), gui_showTrump(), gui_destroyPlayerCards(), gui_showPlayerCards().

--- src/WhistGameGUI/gui.c
+++ src/WhistGameGUI/gui.c
@@ -5,6 +5,8 @@
 
 #include "gui.h"
 
+#include <string.h>
+
 int gui_init(GtkWidget **window, GtkWidget **fixed, char *title, 
              int width, int height)
 {
@@ -86,14 +88,152 @@ int gui_gameType(GtkWidget *window, GtkWidget *fixed, GtkWidget **label,
     return NO_ERROR;
 }
 
-int gui_setBackground(GtkWidget *window, GtkWidget *fixed, char *pathPicture)
+int gui_setBackground(GtkWidget *fixed, char *pathPicture)
 {
-    if (window == NULL || fixed == NULL || pathPicture == NULL)
+    if (fixed == NULL || pathPicture == NULL)
         return POINTER_NULL;
 
     GtkWidget *image = gtk_image_new_from_file(pathPicture);
     gtk_fixed_put(GTK_FIXED(fixed), image, 0, 0);
     gtk_widget_show(image);
+
+    return NO_ERROR;
+}
+
+int gui_showScore(struct Game *game)
+{
+    printf("A\n");
+    return NO_ERROR;
+}
+
+int gui_createButtonShowScore(GtkWidget *fixed, GtkWidget **showScore,
+                              struct Game *game)
+{
+    if (fixed == NULL)
+        return POINTER_NULL;
+
+    *showScore = gtk_button_new_with_label("Show score");
+    gtk_fixed_put(GTK_FIXED(fixed), *showScore, 620, 50);
+    gtk_widget_show(*showScore);
+    g_signal_connect(G_OBJECT(*showScore), "clicked",
+                     G_CALLBACK(gui_showScore), game);
+
+    return NO_ERROR;
+}
+
+int gui_getPictureName(struct Card *card, char *name)
+{
+    if (card == NULL)
+        return CARD_NULL;
+    if (name == NULL)
+        return POINTER_NULL;
+
+    char suit[2] = {'\0'};
+    switch(card->suit) {
+        case DIAMONDS:
+            suit[0] = 'D';
+            break;
+        case CLUBS:
+            suit[0] = 'C';
+            break;
+        case HEARTS:
+            suit[0] = 'B';
+            break;
+        case SPADES:
+            suit[0] = 'A';
+            break;
+        case SuitEnd:
+            break;
+        }
+    char value[3] = {'\0'};
+    if (card->value > 9) {
+        value[0] = (char)(((int)'0') + card->value / 10);
+        value[1] = (char)(((int)'0') + card->value % 10);
+    } else {
+        value[0] = (char)(((int)'0') + card->value);
+    }
+    strcpy(name, value);
+    strcat(name, suit);
+    strcat(name, ".jpg");
+    
+    return NO_ERROR;
+}
+
+int gui_destroyTrump(GtkWidget **image)
+{
+    if (image == NULL)
+        return POINTER_NULL;
+
+    gtk_widget_destroy(*image);
+
+    return NO_ERROR;
+}
+
+int gui_showTrump(GtkWidget *fixed, struct Card *trump, GtkWidget **image)
+{
+    if (fixed == NULL || image == NULL)
+        return POINTER_NULL;
+
+    char pathTrump[30] = "pictures/45x60/";
+    if (trump == NULL)
+        strcat(pathTrump, "45x60.jpg");
+    else {
+        char pictureName[7];
+        gui_getPictureName(trump, pictureName);
+        strcat(pathTrump, pictureName);
+    }
+
+    *image = gtk_image_new_from_file(pathTrump);
+    gtk_fixed_put(GTK_FIXED(fixed), *image, 45, 15);
+    gtk_widget_show(*image);
+
+    return NO_ERROR;
+}
+
+int gui_destroyPlayerCards(struct PlayerCards *playerCards)
+{
+    if (playerCards == NULL)
+        return POINTER_NULL;
+
+    for (int i = 0; i < MAX_CARDS; i++) {
+        if (playerCards->images[i] != NULL) {
+            gtk_widget_destroy(playerCards->images[i]);
+            playerCards->images[i] = NULL;
+        }
+        if (playerCards->event_box[i] != NULL) {
+            gtk_widget_destroy(playerCards->event_box[i]);
+            playerCards->event_box[i] = NULL;
+        }
+    }
+
+    return NO_ERROR;
+}
+
+int gui_showPlayerCards(struct PlayerCards *playerCards, GtkWidget *fixed,
+                        struct Player *player)
+{
+    if (playerCards == NULL || fixed == NULL)
+        return POINTER_NULL;
+    if (player == NULL)
+        return PLAYER_NULL;
+
+    int noOfCards = 0;
+    for (int i = 0; i < MAX_CARDS; i++)
+        if (player->hand[i] != NULL) {
+            char pictureName[10] = {'\0'};
+            gui_getPictureName(player->hand[i], pictureName);
+            char pathImage[30] = "pictures/80x110/";
+            strcat(pathImage, pictureName);
+            playerCards->images[i] = gtk_image_new_from_file(pathImage);
+            playerCards->event_box[i] = gtk_event_box_new();
+            gtk_container_add(GTK_CONTAINER(playerCards->event_box[i]), 
+                              playerCards->images[i]);
+            gtk_fixed_put(GTK_FIXED(fixed), playerCards->event_box[i],
+                          10 + noOfCards * 90, 400);
+            gtk_widget_show(playerCards->event_box[i]);
+            gtk_widget_show(playerCards->images[i]);
+            noOfCards++;
+        }
 
     return NO_ERROR;
 }
