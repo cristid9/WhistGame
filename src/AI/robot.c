601160cd95733b280c5cc075021db89efd2937d4
EnachescuAlin <alinenachescu033@gmail.com>
2014-08-04 01:34:10 +0300

    Removed the code used to searching of a bug.

--- src/AI/robot.c
+++ src/AI/robot.c
@@ -75,14 +75,7 @@ int robot_getCardId(struct Player *player, struct Round *round)
     if (playerId < 0)
         return playerId;
 
-    static int a = 0;
-    a++;
-    for (int i = 0; i < MAX_GAME_PLAYERS; i++)
-        printf("%s ", round->hand->players[i]->name);
-    printf("\n");
     if (round->hand->players[0] == player) {
-   // if (a == 6) {
-        printf("test");
         if (round->bids[playerId] > round->handsNumber[playerId]) {
             int max = 0;
             int position = 0;
